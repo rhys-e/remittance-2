456128f862f07c2fa9a238b2778aa137ca76b49a
rhys-e <1895732+rhys-e@users.noreply.github.com>
2018-03-11 23:16:42 +0000

    added method for easy hash construction. Also added contract expiration, after which only the owner can withdraw

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.19;
 contract Remittance {
 
   bytes32 private hash;
+  uint public expiration;
   address public owner;
   address public recipient;
 
@@ -19,14 +20,15 @@ contract Remittance {
   // Also including Carol's one-time password (as provided by Alice)
   // otherwise we're building a bit of a rainbow table for Carol the more
   // txs she handles, assuming Alice's allow her to re-use her address.
-  function Remittance(address _recipient, bytes32 _hash) public payable {
+  function Remittance(address _recipient, bytes32 _hash, uint _expiration) public payable {
     require(msg.value > 0);
-    require(recipient != address(0));
-    require(recipient != msg.sender);
+    require(_recipient != address(0));
+    require(_recipient != msg.sender);
 
     owner = msg.sender;
     recipient = _recipient;
     hash = _hash;
+    expiration = _expiration + block.number;
 
     LogCreated(owner, recipient, msg.value);
   }
@@ -35,15 +37,28 @@ contract Remittance {
   // be released to the intended recipient. Hash is more involved than
   // just keccak256(bobsPw) to avoid nasty Carol's rainbow tabling
   function withdraw(bytes32 pw1, bytes32 pw2) public returns(bool) {
-    require(msg.sender == recipient);
-    require(keccak256(recipient, owner, pw1, pw2) == hash);
-
     uint balance = address(this).balance;
+
+    if (block.number > expiration) {
+      require(msg.sender == owner);
+      owner.transfer(balance);
+    } else {
+      require(msg.sender == recipient);
+      require(getHash(recipient, owner, pw1, pw2) == hash);
+
+      recipient.transfer(balance);
+    }
+
     LogWithdraw(recipient, balance);
 
-    recipient.transfer(balance);
-
     return true;
+  }
+
+  function getHash(address recipAddr, address ownerAddr, bytes32 pw1, bytes32 pw2)
+    public
+    pure
+    returns (bytes32) {
+      return keccak256(recipAddr, ownerAddr, pw1, pw2);
   }
 
   function invalidate() public onlyOwner returns (bool) {
