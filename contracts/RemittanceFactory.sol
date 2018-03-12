c2c1b5c7708605524efe7066692ea1f18b5d8295
rhys-e <1895732+rhys-e@users.noreply.github.com>
2018-03-12 23:27:40 +0000

    first pass at taking a fee for the contract owner

--- contracts/RemittanceFactory.sol
+++ contracts/RemittanceFactory.sol
@@ -5,24 +5,38 @@ import "./Remittance.sol";
 contract RemittanceFactory {
 
   address[] public contracts;
+  address public owner;
+  uint private gasFee = 53000; // tx fee + contract create fee
 
   event LogNewRemittance(
     address indexed owner,
     address indexed recipient,
+    address indexed remittanceAddr,
     uint amount,
     uint expiration);
 
-  function RemittanceFactory() public {}
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  function RemittanceFactory() public {
+    owner = msg.sender;
+  }
 
   function newRemittance(address recipient, bytes32 hash, uint expiration)
     public
     payable
     returns (address) {
+
+      uint fee = tx.gasprice * gasFee;
+      require(msg.value > fee);
+
       // should we be passing in the owner rather than using msg.sender?
-      Remittance r = (new Remittance).value(msg.value)(msg.sender, recipient, hash, expiration);
+      Remittance r = (new Remittance).value(msg.value - fee)(msg.sender, recipient, hash, expiration);
       contracts.push(r);
 
-      LogNewRemittance(msg.sender, recipient, msg.value, expiration + block.number);
+      LogNewRemittance(msg.sender, recipient, r, msg.value, expiration + block.number);
       return r;
   }
 
@@ -31,6 +45,12 @@ contract RemittanceFactory {
     pure
     returns (bytes32) {
       return keccak256(recipAddr, ownerAddr, pw1, pw2);
+  }
+
+  function withdraw()
+    public
+    onlyOwner {
+      owner.transfer(address(this).balance);
   }
 
   function() private {}
