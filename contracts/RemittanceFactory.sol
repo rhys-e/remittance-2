4e53482595a8b396ed009f38483611dea3736715
rhys-e <1895732+rhys-e@users.noreply.github.com>
2018-03-12 23:36:22 +0000

    added fee and pause/resume

--- contracts/RemittanceFactory.sol
+++ contracts/RemittanceFactory.sol
@@ -7,6 +7,8 @@ contract RemittanceFactory {
   address[] public contracts;
   address public owner;
   uint private gasFee = 53000; // tx fee + contract create fee
+  uint private accumulatedFee = 0;
+  bool private active = true;
 
   event LogNewRemittance(
     address indexed owner,
@@ -15,8 +17,22 @@ contract RemittanceFactory {
     uint amount,
     uint expiration);
 
+  event LogPaused();
+  event LogResumed();
+  event LogWithrawFee(uint fee);
+
   modifier onlyOwner() {
     require(msg.sender == owner);
+    _;
+  }
+
+  modifier isActive() {
+    require(active == true);
+    _;
+  }
+
+  modifier isInactive() {
+    require(active == false);
     _;
   }
 
@@ -29,9 +45,11 @@ contract RemittanceFactory {
     payable
     returns (address) {
 
+      require(active == true);
       uint fee = tx.gasprice * gasFee;
       require(msg.value > fee);
 
+      accumulatedFee += fee;
       // should we be passing in the owner rather than using msg.sender?
       Remittance r = (new Remittance).value(msg.value - fee)(msg.sender, recipient, hash, expiration);
       contracts.push(r);
@@ -47,10 +65,28 @@ contract RemittanceFactory {
       return keccak256(recipAddr, ownerAddr, pw1, pw2);
   }
 
-  function withdraw()
+  function resume()
+    public
+    onlyOwner
+    isInactive {
+      active = true;
+      LogResumed();
+  }
+
+  function pause()
+    public
+    onlyOwner
+    isActive {
+      active = false;
+      LogPaused();
+  }
+
+  function withdrawFee()
     public
     onlyOwner {
-      owner.transfer(address(this).balance);
+      LogWithrawFee(accumulatedFee);
+      accumulatedFee = 0;
+      owner.transfer(accumulatedFee);
   }
 
   function() private {}
