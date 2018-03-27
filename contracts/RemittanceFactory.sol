0c033ff60d32cc429c80bca5af0213b84f3668c2
rhys-e <1895732+rhys-e@users.noreply.github.com>
2018-03-27 23:04:27 +0100

    added contract control methods

--- contracts/RemittanceFactory.sol
+++ contracts/RemittanceFactory.sol
@@ -6,7 +6,7 @@ import "./Stoppable.sol";
 
 contract RemittanceFactory is Stoppable, PasswordVerifier {
 
-  address[] public remittanceContracts;
+  mapping(address => bool) private remittanceExists;
   uint private gasFee = 53000; // tx fee + contract create fee
   uint private accumulatedFee = 0;
 
@@ -18,12 +18,13 @@ contract RemittanceFactory is Stoppable, PasswordVerifier {
     uint expiration);
 
   event LogWithdrawFee(address indexed sender, uint fee);
+  event LogPauseRemittance(address indexed sender, address remittance);
+  event LogResumeRemittance(address indexed sender, address remittance);
+  event LogNewRemittanceOwner(address indexed sender, address indexed newOwner, address remittance);
 
-  function remittanceContractCount()
-    public
-    view
-    returns(uint count) {
-    return remittanceContracts.length;
+  modifier onlyIfRemittance(address remittance) {
+    require(remittanceExists[remittance] == true);
+    _;
   }
 
   function RemittanceFactory()
@@ -40,10 +41,41 @@ contract RemittanceFactory is Stoppable, PasswordVerifier {
 
       accumulatedFee += fee;
       Remittance trustedRemittance = (new Remittance).value(msg.value - fee)(msg.sender, recipient, hash, expiration);
-      remittanceContracts.push(trustedRemittance);
+      remittanceExists[trustedRemittance] = true;
+      // ideally would log this before, but obviously can't log the contract address before it's created
       LogCreateRemittance(msg.sender, recipient, trustedRemittance, msg.value, expiration + block.number);
 
       return trustedRemittance;
+  }
+
+  function changeRemittanceOwner(address remittance, address newOwner)
+    public
+    onlyOwner
+    onlyIfRemittance(remittance)
+  {
+    Remittance trustedRemittance = Remittance(remittance);
+    LogNewRemittanceOwner(msg.sender, newOwner, trustedRemittance);
+    trustedRemittance.changeOwner(newOwner);
+  }
+
+  function resumeRemittance(address remittance)
+    public
+    onlyOwner
+    onlyIfRemittance(remittance)
+  {
+    Remittance trustedRemittance = Remittance(remittance);
+    LogResumeRemittance(msg.sender, trustedRemittance);
+    trustedRemittance.resume();
+  }
+
+  function pauseRemittance(address remittance)
+    public
+    onlyOwner
+    onlyIfRemittance(remittance)
+  {
+    Remittance trustedRemittance = Remittance(remittance);
+    LogPauseRemittance(msg.sender, trustedRemittance);
+    trustedRemittance.pause();
   }
 
   function getFeeAmount()
